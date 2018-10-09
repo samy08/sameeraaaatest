694eb19723c66926009543a0bdd75004eeb2f430
sameera <samterra85@gmail.com>
2018-10-10 00:08:00 +0530

    PrimeNumberCommit

---
+++ JJJUnit/src/main/java/com/example/java/PrimeNumber.java
@@ -0,0 +1,28 @@
+package com.example.java;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+public static void main(String[] args) {
+	/*Scanner sc=new Scanner(System.in);*/
+ 	int n=3;
+	int c=0;
+	for(int i=1;i<=n;i++) {
+		
+		
+		if(n%i==0) {
+			if(i==1 && i==n)
+			c++;
+			System.out.println(c);
+		}
+		
+		}
+	if(c==2) {
+		System.out.println("entered number is prime"+c);
+	}
+	else {
+		System.out.println("entered number is not prime"+c);
+	}
+	}
+}
+
